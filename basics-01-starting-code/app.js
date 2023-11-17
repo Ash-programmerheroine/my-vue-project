bff183388201b7c0cc13e98d2af03252321410b2
Ashrafi <ashrafikhanam.18@gmail.com>
2023-11-17 13:27:00 +0600

    udemy course practice

---
+++ basics-01-starting-code/app.js
@@ -0,0 +1,21 @@
+var app =Vue.createApp({
+    data(){
+        return{
+            courseGoalA:'you should learn vue! !',
+            courseGoalB:'finish the Course and master vue !',
+            vueLink:'https://vuejs.org/',
+        };
+    },
+    methods:{
+        outputGoal(){
+            const randomNum = Math.random();
+            if(randomNum<0.5){
+                return this.courseGoalA;
+            }
+            else{
+                return this.courseGoalB;
+            }
+        }
+    }
+});
+app.mount('#user-goal')
